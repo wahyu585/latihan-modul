d3157c5ef12eb817402c28efa417af813edbaf4d
wahyu585 <wahyu_risky_29rpl@student.smktelkom-mlg.sch.id>
2022-04-28 14:16:17 +0700

    react 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Alert from './Components/Alert';
+import Media from './Components/Media';
+// import element Alert yang telah dibuat
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    type: "danger",
+    header: "Fatal Error",
+    content: "ini content dari alert"
+    }
+    }
+    // fungsi untuk mengubah tipe
+ changeTypeAlert = (event) =>{
+ this.setState({type: event.target.value});
+  }
+   // fungsi untuk mengubah header
+  changeHeaderAlert = (event) => {
+   this.setState({header: event.target.value});
+  }
+  // fungsi untuk mengubah content
+  changeContentAlert = (event) => {
+   this.setState({content: event.target.value});
+ }
+render(){
+  
+  
+return (
+      <div className="App container col-sm-6">
+      <Alert type={this.state.type} header={this.state.header}>
+{this.state.content}
+</Alert>  
+<hr />
+<h4>Alert Control</h4>
+<b className="text-left">Tipe Alert</b>
+<select className="form-control" name="type" value={this.state.type}
+onChange={this.changeTypeAlert}>
+<option value="success">Success</option>
+<option value="warning">Warning</option>
+<option value="danger">Danger</option>
+ <option value="info">Info</option>
+</select>
+ <b className="text-left">Header Alert</b>
+ <input
+ type="text" name="header" className="form-control" value={this.state.header}
+onChange={this.changeHeaderAlert} />
+<b className="text-left">Content Alert</b>
+<input
+type="text" name="content" className="form-control" value={this.state.content}
+ onChange={this.changeContentAlert} />
+      </div>
+      
+      );
+      
+      
+}   
 }
 
 export default App;
